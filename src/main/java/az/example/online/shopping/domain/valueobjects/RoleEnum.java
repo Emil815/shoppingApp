b79fd541e489749828238ef1bae9434f404ae60a
emil.mammadov <emil.mammadov@cybernet.az>
2024-11-17 14:08:12 +0400

    manifest

--- src/main/java/az/example/online/shopping/domain/valueobjects/RoleEnum.java
+++ src/main/java/az/example/online/shopping/domain/valueobjects/RoleEnum.java
@@ -1,5 +1,15 @@
 package az.example.online.shopping.domain.valueobjects;
 
+import lombok.Getter;
+
+@Getter
 public enum RoleEnum {
-    ROLE_ADMIN, ROLE_MODERATOR, ROLE_USER
+    ROLE_ADMIN("ADMIN"),
+    ROLE_MODERATOR("MODERATOR"),
+    ROLE_USER("USER");
+    private final String role;
+    RoleEnum(String role) {
+        this.role = role;
+    }
+
 }
